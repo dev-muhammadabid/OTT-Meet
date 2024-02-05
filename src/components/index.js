31c0313970420d061ff970148227727ab868e75e
dev-muhammadabid <muhammadabid.dev@gmail.com>
2024-02-05 21:47:41 +0530

    New Feat

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
 export { default as Home } from "./homeSection/Home";
-export { default as Movies } from "./moviesSection/Movies";
+export { default as Youtube } from "./youtubeSection/Youtube";
 export { default as Omeet } from "./omeetSection/Omeet";
 export { default as Login } from "./loginSection/Login";
 export { default as Signup } from "./signupSection/Signup";
